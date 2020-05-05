b9a611025b9e00bc4f1881c0e74558a09aecfe79
atom <hokkaido.kg.gup@gmail.com>
2020-05-05 20:26:32 +0900

    add gitignore

--- old/117-156/ABC117/A.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void){
-    int x,t;
-    scanf("%d %d",&t,&x);
-    printf("%f\n", (double)t/x);
-    return 0;
-}
--- old/_ABC過去/081PM.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-    int s[3];
-    scanf("%1d%1d%1d",&s[0],&s[1],&s[2]);
-    printf("%d\n", s[0]+s[1]+s[2]);
-    return 0;
-}
